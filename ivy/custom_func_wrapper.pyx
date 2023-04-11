5d1f8170394d5a1cf3f4119930e132d925af4781
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2023-04-11 12:48:02 +0000

    Added some more hardcoded logic to give a 59% improvement in speed

--- ivy/custom_func_wrapper.pyx
+++ ivy/custom_func_wrapper.pyx
@@ -1,26 +1,6 @@
 from typing import Union, Optional, Callable, Dict
 import functools
 import ivy
-import cython
-
-
-cdef get_array_mode():
-    return ivy.get_array_mode()
-
-
-cdef args_to_native(
-    x1: ivy.Array,
-    x2: ivy.Array,
-    include_derived: Dict[type, bool] = None,
-    cont_inplace: bool = False,
-):
-    return ivy.args_to_native(x1, x2, include_derived=include_derived, cont_inplace=cont_inplace)
-
-
-cdef res_ret(ret, nested=True, include_derived=None):
-    if get_array_mode():
-        return ivy.to_ivy(ret, nested=nested, include_derived=include_derived)
-    return ret
 
 
 def add_wrapper(fn: Callable):
@@ -31,31 +11,8 @@ def add_wrapper(fn: Callable):
         alpha: float = None,
         out: ivy.Array = None,
     ):
-        # currently restricted to comparing based on the to_native_arrays_and_back wrapper only
-        # outputs_to_ivy_arrays
-
-        # call unmodified function
-
-        # inputs_to_native_arrays
-
-        if not get_array_mode():
-            ret: ivy.Array = fn(x1, x2, alpha=alpha, out=out)
-        else:
-            # check if kwargs contains an out argument, and if so, remove it
-            has_out: bool = False
-            out_temp = None
-            if out is not None:
-                out_temp = out
-                has_out = True
-            # convert all arrays in the inputs to ivy.NativeArray instances
-            (x1, x2), _ = args_to_native(x1, x2)
-            # add the original out argument back to the keyword arguments
-            ret: ivy.Array = fn(x1, x2, alpha=alpha, out=out_temp)
-        
-        # -----------------------
-
-        # convert all arrays in the return to `ivy.Array` instances
-        return res_ret(ret, nested=True, include_derived={tuple: True})
-        # -----------------------
+        x1, x2 = x1._data, x2._data
+        ret: ivy.Array = fn(x1, x2, alpha=alpha)
+        return ivy.asarray(ret)
     new_fn.add_wrapper = True
     return new_fn
